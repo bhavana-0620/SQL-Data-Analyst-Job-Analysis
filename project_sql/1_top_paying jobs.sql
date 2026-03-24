9ceb3b29c4eac34704e30327400aa07a5cf38e86
bhavana-0620 <bhavanabangaru78@gmail.com>
2026-03-24 18:55:06 +0530

    Updated_top_paying jobs.sql

--- project_sql/1_top_paying jobs.sql
+++ project_sql/1_top_paying jobs.sql
@@ -1,25 +1,41 @@
+-- 01_top_paying_jobs.sql
+
 /*
 What are the top paying data analyst jobs? 
-Identify the top 10 highest paying data analyst roles that are available remotely. 
-Focus on job postings with specified salaries (Remove nulls). 
-Why? Highlight the top paying opportunities for data analysts, offering insights into the employment category.
+- Only Data Analyst roles
+- Location - Anywhere 
+- Remove null salaries
 */
 
-SELECT
+WITH ranked_jobs AS (
+    SELECT
+        jp.job_id,
+        jp.job_title,
+        jp.job_location,
+        jp.job_schedule_type,
+        jp.salary_year_avg,
+        jp.job_posted_date,
+        cd.name AS company_name,
+        ROW_NUMBER() OVER (ORDER BY jp.salary_year_avg DESC) AS rank
+    FROM job_postings_fact jp
+    LEFT JOIN company_dim cd 
+        ON jp.company_id = cd.company_id
+    WHERE
+        jp.salary_year_avg IS NOT NULL
+        AND jp.job_work_from_home = 1
+        AND jp.job_title LIKE '%Data Analyst%'  
+        AND jp.job_title NOT LIKE '%Director%'   
+        AND jp.job_title NOT LIKE '%Principal%'
+        AND jp.job_title NOT LIKE '%Manager%'
+)
+SELECT 
     job_id,
     job_title,
     job_location,
     job_schedule_type,
     salary_year_avg,
     job_posted_date,
-    name as company_name
-from 
-    job_postings_fact
-left JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
-WHERE
-    job_title_short ='Data Analyst' AND
-    job_location ='Anywhere' AND
-    salary_year_avg is not NULL
-ORDER BY
-    salary_year_avg DESC
-LIMIT 10
+    company_name
+FROM ranked_jobs
+WHERE rank <= 10
+ORDER BY salary_year_avg DESC;
