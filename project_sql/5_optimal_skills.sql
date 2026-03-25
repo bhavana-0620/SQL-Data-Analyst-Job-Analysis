42785f96d892b6676cfb489a44410df080d53953
bhavana-0620 <bhavanabangaru78@gmail.com>
2026-03-26 00:01:18 +0530

    Updated_optimal_skills.sql

--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -1,44 +1,40 @@
 -- 05_optimal_skills.sql
 
 /*
-Most optimal skills (high demand + high salary)
+Goal: Identify "Optimal" skills—those that are both high-demand and high-salary.
+- Combines demand (Count) and value (Avg Salary) into a single analysis.
+- Filters for remote roles (Anywhere) to keep the comparison consistent.
+- Demonstrates advanced multi-metric aggregation in a single query.
 */
 
-WITH filtered_jobs AS (
-    SELECT 
-        job_id,
-        salary_year_avg
-    FROM job_postings_fact
-    WHERE 
-        salary_year_avg IS NOT NULL
-        AND job_title LIKE '%Data Analyst%'
-        AND job_title NOT LIKE '%Director%'
-        AND job_title NOT LIKE '%Principal%'
-        AND job_title NOT LIKE '%Manager%'
-),
-
-skill_stats AS (
+WITH skill_metrics AS (
     SELECT 
         sd.skill_id,
         sd.skills,
-        COUNT(DISTINCT fj.job_id) AS demand_count,
-        AVG(fj.salary_year_avg) AS avg_salary
-    FROM filtered_jobs fj
-    JOIN skills_job_dim sj 
-        ON fj.job_id = sj.job_id
-    JOIN skills_dim sd 
-        ON sj.skill_id = sd.skill_id
-    GROUP BY sd.skill_id, sd.skills
+        COUNT(sjd.job_id) AS demand_count,
+        AVG(jp.salary_year_avg) AS avg_salary
+    FROM 
+        job_postings_fact AS jp
+    INNER JOIN skills_job_dim AS sjd 
+        ON jp.job_id = sjd.job_id
+    INNER JOIN skills_dim AS sd 
+        ON sjd.skill_id = sd.skill_id
+    WHERE 
+        jp.job_title_short = 'Data Analyst' 
+        AND jp.salary_year_avg IS NOT NULL
+        AND jp.job_work_from_home = TRUE
+    GROUP BY 
+        sd.skill_id
 )
 
 SELECT 
-    skill_id,
     skills,
     demand_count,
     ROUND(avg_salary, 0) AS avg_salary
-FROM skill_stats
-WHERE demand_count >= 5  
-ORDER BY 
-    demand_count DESC,
-    avg_salary DESC
-    limit 25;
+FROM 
+    skill_metrics
+WHERE 
+    demand_count > 10 
+    avg_salary DESC, 
+    demand_count DESC
+LIMIT 25;
